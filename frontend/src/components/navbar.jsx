da4a9a289aae9c7e8dec576101a8b2a7077b08e3
Soham <sohamx0@users.noreply.github.com>
2026-03-28 19:35:14 +0530

    added dropdown menu option for smaller screens

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
 import { assets } from '../assets/assets.js'
 import { Link, NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(false)
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.logo} alt='Logo' />
@@ -45,6 +46,16 @@ const Navbar = () => {
                 <img src={assets.cart_icon} className='w-5 min-w-5' alt='Cart'></img>
                 <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
             </Link>
+            <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt='Menu'></img>
+        </div>
+        {/*sidebar menu for smaller screens*/}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white trasition-all ${visible ? 'w-full' : 'w-0'}`}>
+            <div className='flex flex-col text-grey-600 '>
+                <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img className='h-4 rotate-180' src={assets.dropdown_icon} alt='Logo'></img>    
+                    <p>Back</p>
+                </div>
+            </div>
         </div>
     </div>
   )
