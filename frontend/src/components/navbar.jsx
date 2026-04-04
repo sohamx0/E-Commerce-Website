58ce761e073e699737879f103792f7996c3fe004
Soham <sohamx0@users.noreply.github.com>
2026-04-04 14:33:04 +0530

    added navigation links to Navbar component

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -55,6 +55,10 @@ const Navbar = () => {
                     <img className='h-4 rotate-180' src={assets.dropdown_icon} alt='Logo'></img>    
                     <p>Back</p>
                 </div>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>Home</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>Collection</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>About</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>Contact</NavLink>
             </div>
         </div>
     </div>
